6e85364bf4de225af072fba9483c8cc4846d4c2d
Your Name <you@example.com>
2023-03-31 00:30:01 +0200

    g

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,11 +1,18 @@
 import { Request, RequestType } from './testt'
 
 const a = () => {
+    var json = JSON.parse("{\"Type\":\"READ\",\"First\":\"ee\"}")
+    var a = Request.fromJSON(json)
+    console.log(Request.toJSON(a))
     var a = Request.create({
         Type: RequestType.WRITE,
-        Params: { First: "e", $case: "First" },
+        Params: {
+            Second: "secondary",
+            $case: "Second"
+        }
         //First: "test"
     })
-    console.log(JSON.stringify(Request.toJSON(a)))
+    console.log(Request.toJSON(a))
+
 }
 a()
